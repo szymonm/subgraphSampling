0de62ef1b72f15b2033ec2bf6f40505b0976f376
Szymon Matejczyk <smatejczyk@gmail.com>
2015-05-01 09:01:24 +0200

    Simulations setting

--- src/main/scala/pl/szymonmatejczyk/myerson/MyersonApproximationComparison.scala
+++ src/main/scala/pl/szymonmatejczyk/myerson/MyersonApproximationComparison.scala
@@ -1,7 +1,7 @@
 package pl.szymonmatejczyk.myerson
 
 import com.twitter.cassovary.graph.{DirectedGraph, TestGraphs}
-import pl.szymonmatejczyk.generation.AdditionalTestGraphs
+import pl.szymonmatejczyk.generation.{PreferentialAttachment, AdditionalTestGraphs}
 import purecsv.safe._
 
 import scala.collection.mutable.ArrayBuffer
@@ -12,9 +12,9 @@ object MyersonApproximationComparison {
   case class Result(method: String, graph: String, function: String, samples: Int, error: Double)
 
   def mapDiff(m1: collection.Map[Int, Double], m2: collection.Map[Int, Double],
-              errorFunction: (Double => Double) = x => x * x): Double = {
+              errorFunction: (Double => Double) = x => x): Double = {
     require(m1.size == m2.size, "Map sizes do not fit")
-    m1.iterator.map{ case (k, v) => errorFunction(math.abs( v - m2(k) ))}.sum
+    m1.iterator.map{ case (k, v) => errorFunction( math.abs( v - m2(k) ))}.sum
   }
 
   def compare(graph: DirectedGraph, method: PowerIndexComputation,
@@ -38,27 +38,31 @@ object MyersonApproximationComparison {
 
   def compute(): Seq[Result] = {
     val rand = new Random
-    val agents = 15
+    val agents = 30
     //    val graphs = List(TestGraphs.generateRandomUndirectedGraph(agents, 0.4))
     val graphs = List(
-//      (AdditionalTestGraphs.mutualCycle(agents), "cycle")//,
-      (TestGraphs.generateRandomUndirectedGraph(agents, 0.4), "ER(0.4)")
+      (AdditionalTestGraphs.mutualCycle(agents), "cycle"),
+      (TestGraphs.generateRandomUndirectedGraph(agents, 0.4), "ER(0.4)"),
+      (new PreferentialAttachment(agents, 4, 3, rand).apply(), s"PA($agents, 4, 3)")
     )
 
-    val samplings = List(100, 200, 300, 500, 1000, 1500, 2000, 3000, 4000, 5000, 7000, 10000)
+    val samplings = List(100, 200, 300, 500, 1000, 1500, 2000, 3000, 4000, 5000, 7000, 10000, 15000)
 
     def valuationFunctions(g : DirectedGraph) =
-      List[MyersonValuation](socc(g, socc(g, new SizeValuation)))//RandomValuationsGenerator.uniform(g.nodeCount)))
+      List[MyersonValuation](socc(g, socc(g, new SizeValuation)), socc(g, RandomValuationsGenerator.uniform(g.nodeCount)),
+        socc(g, RandomValuationsGenerator.superadditiveUniform(agents, 3.0)))
 
     def methods(g: DirectedGraph, v: MyersonValuation, s: Int) = List[IterativePowerIndexComputation](
       //      new ExactMyersonValue(g, v),
       new SamplingCoalitionsMyersonValue(g, v, s)
-      ,new HybridCoalitionsMyersonValue(g, v, s, 4)
+      ,new HybridCoalitionsMyersonValue(g, v, s, 2)
       //      new MyersonFromCCGenerator(g, RandomCoalitionGenerator(g, new PerfectSampling(g, rand), s), v),
       ,new SumCCBasedSamplingCoalitionsMyersonValue(g, v, s)
     )
 
     val results = ArrayBuffer[Result]()
+
+    val avg = 10
 
     for {
       g <- graphs
@@ -73,10 +77,20 @@ object MyersonApproximationComparison {
 //        val r1 = compare(g._1, method, exact)
 //        println(method.getClass.getCanonicalName + ": " + r1._1)
 //        println(r1._2)
-        val r = compareSamplings(g._1, method, samplings, exact)
-        samplings.zip(r).foreach {
+
+        def mean(s : Array[Double]) = s.sum / s.size
+
+        val resArray: Array[Array[Double]] = Array.fill(samplings.size, avg)(0.0)
+
+        for (i <- 0 until avg) {
+          compareSamplings(g._1, method, samplings, exact).zipWithIndex.foreach {
+            case (a, index) => resArray(index)(i) = a
+          }
+        }
+
+        samplings.zip(resArray.map(mean)).foreach {
           case (iterations, error) =>
-            println(s"${method.getClass.getSimpleName}\t$iterations\t$error")
+            println(s"${method.getClass.getSimpleName}\t${g._2}\t$iterations\t$error")
             results += Result(method.getClass.getSimpleName, g._2, v.name, iterations, error)
         }
       }
